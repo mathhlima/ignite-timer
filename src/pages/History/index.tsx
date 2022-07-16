89cb5f851e08932f517a172c742940da85ee6190
Matheus <matheus_sousa@protonmail.com>
2022-07-16 16:36:13 -0300

    feat: component status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -20,28 +20,36 @@ export function History() {
               <td>tarefa</td>
               <td>20 minutos</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>tarefa</td>
               <td>20 minutos</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
 
             <tr>
               <td>tarefa</td>
               <td>20 minutos</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído </Status>
+              </td>
             </tr>
 
             <tr>
               <td>tarefa</td>
               <td>20 minutos</td>
               <td>Há dois meses</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
           </tbody>
         </table>
